0ce1e3b9638b1f776cf962cc2fae3116da75d9f7
vashe25 <va.she25@gmail.com>
2016-03-23 01:12:17 +0300

    final tweaks in Finder

--- finder.php
+++ finder.php
@@ -1,25 +1,35 @@
 <?php
-`chcp 65001`;
-
 /**
-* Link Finder
+* Finder
 */
-class linkFinder 
+class Finder 
 {
 	
 	function __construct()
 	{
-		
+		if (isset($argv[1])) {
+			$this->pattern = $argv[1];
+		}
 	}
-
+	//file for links
 	public $linksFile = "links.txt";
+	//array of links
+	public $linksArray = array("http://moscow.megafon.ru/");
+	//regexp pattern
+	public $pattern = "href=\"(.+\.pdf)\"";
+	//all pages content
+	public $pages = array();
+	//array for all finded matches
+	public $matches = array();
+	//filename for writing result
+	public $logFile = "log_LF.txt";
 
-	public function loadFile($filename = "links.txt") {
-		if (file_exists($filename)) {
-			$array = file($filename, FILE_SKIP_EMPTY_LINES);
-			return $array;
+	public function loadFile() {
+		if (file_exists($this->linksFile)) {
+			$this->linksArray = file($this->linksFile, FILE_SKIP_EMPTY_LINES | FILE_IGNORE_NEW_LINES);
+			return TRUE;
 		} else {
-			$fp = fopen($filename, "w");
+			$fp = fopen($this->linksFile, "w");
 			$string = "http://moscow.megafon.ru/";
 			fwrite($fp, $string);
 			fclose($fp);
@@ -27,55 +37,57 @@ class linkFinder
 		}
 	}
 
-	public function getPage($array = array("http://moscow.megafon.ru/"))
+	public function getPage()
 	{
-		$pages = array();
-		$ch = curl_init();
-		foreach ($array as $link) {
+		foreach ($this->linksArray as $link) {
+			$ch = curl_init();
 			curl_setopt($ch, CURLOPT_URL, $link);
 			curl_setopt($ch, CURLOPT_FRESH_CONNECT, TRUE);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-			$pages[$link] = curl_exec($ch);
+			$this->pages[$link] = curl_exec($ch);
+			curl_close($ch);
 		}
-		curl_close($ch);
-		return $pages;
 	}
 
-	public function findPattern($pages = array(), $pattern = "\".+.pdf\"")
+	public function findPattern()
 	{
-		$pattern = "{" . $pattern . "}";
-		$matches = array();
-		foreach ($pages as $link => $content) {
-			preg_match_all($pattern, $content, $matches[$link], PREG_SET_ORDER);
+		$pattern = "{" . $this->pattern . "}";
+		foreach ($this->pages as $link => $content) {
+			preg_match_all($pattern, $content, $this->matches[$link], PREG_SET_ORDER);
 		}
-		return $matches;
 	}
 
-	public function printResult($matches = array())
+	public function printResult()
 	{	
 		$string = "Finded results:\r\n\r\n";
-		foreach ($matches as $link => $match) {
+		foreach ($this->matches as $link => $match) {
 			$string .= $link . "\r\n";
-			foreach ($match as $value) {
-				$string .= "	" . $value . "\r\n";
+			if ($match) {
+				foreach ($match as $value) {
+					$string .= "	" . $value[0] . "\r\n";
+				}
+			} else {
+				$string .= "	" . "NOTHING" . "\r\n";
 			}
 		}
-		$fp = fopen("finderlog.txt", "w");
+		$fp = fopen($this->logFile, "w");
 		fwrite($fp, $string);
 		fclose($fp);
 	}
+
+	public function run()
+	{
+		if ($this->loadFile()) {
+			$this->getPage();
+			$this->findPattern();
+			$this->printResult();
+			echo "Check results in: " . $this->logFile . "\r\n";
+		} else {
+			echo "Write links in: " . $this->linksFile . "\r\n";
+		}
+	}
 }
 
+$LF = new Finder();
 
-$Obj = new linkFinder();
-
-
-if ($links = $Obj->loadFile())
-{
-	$pages = $Obj->getPage($links);
-
-	$matches = $Obj->findPattern($pages);
-	var_dump($matches);
-	exit;
-	$Obj->printResult($matches);
-}
+$LF->run();
